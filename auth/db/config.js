ff5b16f3aa948d7683f694daee6d9954a8c9ec54
React <react@HP-EliteBook-8460p-b59862e0.localdomain>
2023-06-06 13:57:25 +0300

    project structure set up

---
+++ auth/db/config.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const db = process.env.MONGO_URI
+
+mongoose.connect(db,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+.then(()=>console.log("db connected successfully"))
+.catch((e)=>console.log(e.message))
--- auth/index.js
+++ auth/index.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const cors = require("cors")
+const bodyParse  = require("body-parser")
+require("./db/config")
+
 
 const app = express()
-const port = 5001
+app.use(bodyParse.urlencoded({extended: false}))
+app.use(bodyParse.json())
+
+const port = 4000
 
 app.listen(port, ()=>{
     console.log(`server is running at port ${port}...`)
